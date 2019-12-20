116190fd413c1025add9fb5de0819af458c6a21c
James Flannery <51173206+jflanneryTechtonic@users.noreply.github.com>
2019-12-20 10:13:12 -0700

    Q's 5-8

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,7 +3,15 @@ import React, {Component} from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends React.Component{
-  
+  constructor(props){
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
+  render(){
+    return <div>
+      <div className='QuizQuestion'></div>
+    </div>
+  }
 }
 
 
